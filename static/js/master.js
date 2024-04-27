15c5ac66548ff5fd74dfcd56dc54d3af79644787
sweetandl <51193557+sweetandl@users.noreply.github.com>
2024-04-27 18:03:59 +0800

    01
    01

---
+++ static/js/master.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+
+    //通用tab
+    $('.j-tab .tabhd ul>li:nth-child(1)').addClass("z-sel");
+    $(".j-tab .tabmn>.tabitm:nth-child(1)").show();
+    $('.j-tab .tabhd ul>li').click(function () {
+        $(this).addClass("z-sel").siblings().removeClass();
+        $(this).parents(".j-tab").find(".tabmn>.tabitm").hide().eq($(this).index()).show();
+    });
+
+})
